475465205e128b1392d6c175def54536aebce24a
Kathirvel <115524887+KATHIRVEL-13@users.noreply.github.com>
2024-08-08 20:36:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "devops-shack-s3"
+    bucket         = "terra-learn-bucket"
     key            = "eks.terraform.tfstate"
     region         = "ap-south-1"
     
